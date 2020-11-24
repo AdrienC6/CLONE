3a1ba79e1b8d0c85ba8ef5d22b932935d68255ca
VEAD Development <vead.development@gmail.com>
2020-11-24 13:28:16 +0100

    Add validation

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\Traits\Timestampable;
+use Symfony\Component\Validator\Constraints as Assert; 
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
@@ -25,11 +26,15 @@ class Pin
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le titre du post est obligatoire !")
+     * @Assert\Length(min=3)
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="La description du post est obligatoire!")
+     * @Assert\Length(min=10)
      */
     private $description;
 

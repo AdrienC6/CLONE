8a33d6de6ea316d72fe1367956b59655550729ef
VEAD Development <vead.development@gmail.com>
2020-11-28 19:45:52 +0100

    Add registration

--- migrations/Version20201128160506.php
+++ migrations/Version20201128160506.php
@@ -10,24 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201128150849 extends AbstractMigration
+final class Version20201128160506 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Add pseudo field';
+        return 'Add is_verified field to users table';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users ADD pseudo VARCHAR(20) NOT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E986CC499D ON users (pseudo)');
+        $this->addSql('ALTER TABLE users ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_1483A5E986CC499D ON users');
-        $this->addSql('ALTER TABLE users DROP pseudo');
+        $this->addSql('ALTER TABLE users DROP is_verified');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,16 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="users")
  * @ORM\HasLifecycleCallbacks
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"pseudo"}, message="There is already an account with this pseudo")
+
  */
 class User implements UserInterface
 {
@@ -61,6 +65,11 @@ class User implements UserInterface
      * @ORM\Column(type="string", length=20, unique=true)
      */
     private $pseudo;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -215,6 +224,18 @@ class User implements UserInterface
     public function setPseudo(string $pseudo): self
     {
         $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
